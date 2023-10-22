576cb9ff36184035ad1df74fdc74f899de75e1f8
snow-zen <2372912451zxj@gmail.com>
2023-10-22 17:27:53 +0800

    init project

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Install the script library.
+#
+# Add the 'bin' directory under the project directory to the PATH environment variable.
+
+PROFILE_PATH=~/.profile
+readonly PROFILE_PATH
+SCRIPT_PATH=$(pwd)/bin
+
+case $PATH in
+*:$SCRIPT_PATH:*)
+  # do nothing
+  ;;
+*)
+  if [ -e "$PROFILE_PATH" ]; then
+    touch $PROFILE_PATH
+  fi
+
+  echo "export PATH=\$PATH:$SCRIPT_PATH" | tee -a "$PROFILE_PATH" >/dev/null
+  # shellcheck source=$HOME/.profile
+  source "$PROFILE_PATH"
+  ;;
+esac
