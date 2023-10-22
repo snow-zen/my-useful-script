1cf61f077eb989507bf084ef5ea071bf7fd1702a
snow-zen <2372912451zxj@gmail.com>
2023-10-22 21:25:06 +0800

    fix: setting `PATH` environment variable not effective

--- install.sh
+++ install.sh
@@ -4,9 +4,13 @@
 #
 # Add the 'bin' directory under the project directory to the PATH environment variable.
 
-PROFILE_PATH=~/.profile
+PROFILE_PATH=$1
 readonly PROFILE_PATH
 SCRIPT_PATH=$(pwd)/bin
+
+if [ -z "$PROFILE_PATH" ]; then
+  echo "Please specified environment configuration file path(eg. ~/.bash_profile)" >&2 && exit 1
+fi
 
 case $PATH in
 *:$SCRIPT_PATH:*)
@@ -14,7 +18,7 @@ case $PATH in
   ;;
 *)
   if [ -e "$PROFILE_PATH" ]; then
-    touch $PROFILE_PATH
+    touch "$PROFILE_PATH"
   fi
 
   echo "export PATH=\$PATH:$SCRIPT_PATH" | tee -a "$PROFILE_PATH" >/dev/null
